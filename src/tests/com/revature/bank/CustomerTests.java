dca2d69b20e5a915a60bdcf50115ee6d2b4c25ec
James Taylor <jrtaylor95@gmail.com>
2018-03-03 16:50:42 -0500

    add test for invalid transfers, and removed redundant code

--- src/tests/com/revature/bank/CustomerTests.java
+++ src/tests/com/revature/bank/CustomerTests.java
@@ -20,13 +20,15 @@ public class CustomerTests {
 	@Before
 	public void setUp() throws Exception {
 		customer = new Customer("Customer", "Password");
+		Account expectedAccount = new Account("Checking");
+		expectedAccount.deposit(500);
+		customer.addAccount(expectedAccount);
 	}
 
 	@Test
 	public void testAddAccount() {
 		Account expectedAccount = new Account("Checking");
 		expectedAccount.deposit(500);
-		customer.addAccount(expectedAccount);
 		
 		Account actualAccount = customer.getAccount(0);
 		assertEquals(expectedAccount, actualAccount);
@@ -34,33 +36,24 @@ public class CustomerTests {
 	
 	@Test
 	public void testWithdaw() {
-		Account expectedAccount = new Account("Checking");
-		expectedAccount.deposit(500);
-		customer.addAccount(expectedAccount);
 		assertTrue(customer.withdraw(0, 250));
-		
 		Account actualAccount = customer.getAccount(0);
 		assertEquals(250, actualAccount.getBalance(), 0);
 	}
 	
 	@Test
 	public void testDeposit() {
-		Account expectedAccount = new Account("Checking");
-		customer.addAccount(expectedAccount);
 		assertTrue(customer.deposit(0, 500));
 		
 		Account actualAccount = customer.getAccount(0);
-		assertEquals(500, actualAccount.getBalance(), 0);
+		assertEquals(1000, actualAccount.getBalance(), 0);
 	}
 	
 	@Test
 	public void testTransfer() {
 		Customer transferCustomer = new Customer("Transfer", "Password");
-		Account expectedAccount = new Account("Checking");
 		Account expectedTransferAccount = new Account("Checking");
-		expectedAccount.deposit(500);
 		expectedTransferAccount.deposit(500);
-		customer.addAccount(expectedAccount);
 		transferCustomer.addAccount(expectedTransferAccount);
 		
 		assertTrue(customer.transfer(0, transferCustomer, 0, 250));
@@ -72,18 +65,12 @@ public class CustomerTests {
 	
 	@Test
 	public void testNegativeWithdraw() {
-		Account expectedAccount = new Account("Checking");
-		customer.addAccount(expectedAccount);
-		
 		expectedException.expect(IllegalArgumentException.class);
 		customer.withdraw(0, -100);
 	}
 	
 	@Test
 	public void testNegativeDeposit() {
-		Account expectedAccount = new Account("Checking");
-		customer.addAccount(expectedAccount);
-		
 		expectedException.expect(IllegalArgumentException.class);
 		customer.deposit(0, -100);
 	}
@@ -91,9 +78,7 @@ public class CustomerTests {
 	@Test
 	public void testNegativeTransfer() {
 		Customer transferCustomer = new Customer("Transfer", "Password");
-		Account expectedAccount = new Account("Checking");
 		Account expectedTransferAccount = new Account("Checking");
-		customer.addAccount(expectedAccount);
 		transferCustomer.addAccount(expectedTransferAccount);
 		
 		expectedException.expect(IllegalArgumentException.class);
@@ -102,23 +87,45 @@ public class CustomerTests {
 	
 	@Test
 	public void testOverWithdraw() {
-		Account expectedAccount = new Account("Checking");
-		customer.addAccount(expectedAccount);
-		
 		expectedException.expect(IllegalStateException.class);
-		customer.withdraw(0, 500);
+		customer.withdraw(0, 750);
 	}
 	
 	@Test
 	public void testOverTransfer() {
 		Customer transfer = new Customer("Transfer", "Password");
-		Account expectedAccount = new Account("Checking");
 		Account expectedTransferAccount = new Account("Checking");
-		customer.addAccount(expectedAccount);
 		transfer.addAccount(expectedTransferAccount);
 		
 		expectedException.expect(IllegalStateException.class);
-		customer.transfer(0, transfer, 0, 500);
+		customer.transfer(0, transfer, 0, 750);
+	}
+	
+	@Test
+	public void testTransferToNull() {
+		
+		expectedException.expect(NullPointerException.class);
+		customer.transfer(0, null, 0, 500);
+	}
+	
+	@Test 
+	public void testTransferToNoAccount() {
+		Customer transferCustomer = new Customer("Transfer", "Password");
+		
+		expectedException.expect(IllegalStateException.class);
+		customer.transfer(0, transferCustomer, 0, 500);
+	}
+	
+	@Test
+	public void testTransferOutOfBounds() {
+		Customer transferCustomer = new Customer("Transfer", "Password");
+		Account expectedTransferAccount = new Account("Checking");
+		transferCustomer.addAccount(expectedTransferAccount);
+		
+		expectedException.expect(ArrayIndexOutOfBoundsException.class);
+		customer.transfer(5, transferCustomer, 0, 500);
+		expectedException.expect(ArrayIndexOutOfBoundsException.class);
+		customer.transfer(0, transferCustomer, 5, 500);
 	}
 	
 }
